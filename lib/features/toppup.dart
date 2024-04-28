b5298fb495b394e42a9e80c4b0941f1b16288b07
David Joevincent <221110724@students.mikroskil.ac.id>
2024-04-28 16:29:26 +0700

    Menambahkan tampilan menu top up

--- lib/features/toppup.dart
+++ lib/features/toppup.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/features/banktopup.dart';
+import 'package:flutter_application_1/provider/provider.dart';
+import 'package:provider/provider.dart';
 
-class TopUp extends StatelessWidget {
+class TopUp extends StatefulWidget {
   const TopUp({super.key});
 
+  @override
+  State<TopUp> createState() => _TopUpState();
+}
+
+class _TopUpState extends State<TopUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +27,131 @@ class TopUp extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(
-        color: const Color(0xFF2D3250),
+      backgroundColor: const Color(0xFF2D3250),
+      body: SingleChildScrollView(
+        child: Container(
+          color: const Color(0xFF2D3250),
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Saldo(),
+              Garis(),
+              InkWell(
+                onTap: (){
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => BankTopUp())
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text("BCA",
+                    style: TextStyle(
+                        color: Color(0xFFF6B17A),
+                        fontSize: 30,
+                        fontFamily: 'Neue'
+                    ),
+                  ),
+                    Image(image: AssetImage("assets/logo_BCA.png"),width: 60,fit: BoxFit.cover,)
+                  ],
+                ),
+              ),
+              SizedBox(height: 10,),
+              InkWell(
+                onTap: (){
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => BankTopUp())
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text("BRI",
+                    style: TextStyle(
+                        color: Color(0xFFF6B17A),
+                        fontSize: 30,
+                        fontFamily: 'Neue'
+                    ),
+                  ),
+                    Image(image: AssetImage("assets/Logo_BRI.png"),width: 60,fit: BoxFit.cover,)
+                  ],
+                ),
+              ),
+              SizedBox(height: 10,),
+              InkWell(
+                onTap: (){
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => BankTopUp())
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text("Mandiri",
+                    style: TextStyle(
+                        color: Color(0xFFF6B17A),
+                        fontSize: 30,
+                        fontFamily: 'Neue'
+                    ),
+                  ),
+                    Image(image: AssetImage("assets/Logo_Mandiri.png"),width: 60,fit: BoxFit.cover,)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+class Saldo extends StatefulWidget {
+  const Saldo({super.key});
+
+  @override
+  State<Saldo> createState() => _SaldoState();
+}
+
+class _SaldoState extends State<Saldo> {
+  @override
+  Widget build(BuildContext context) {
+    final _saldo = Provider.of<SaldoProvider>(context,listen: false).saldo;
+    return SizedBox(
+      child: (
+          Row(
+            children: [
+              const Text("Saldo",
+                style: TextStyle(
+                    color: Color(0xFFF6B17A),
+                    fontSize: 30,
+                    fontFamily: 'Neue'
+                ),
+              ),
+              const SizedBox(width: 10,),
+              Text("Rp${_saldo.toStringAsFixed(0)}",
+                style: const TextStyle(
+                    color: Color(0xFFFFFFFF),
+                    fontSize: 30,
+                    fontFamily: 'Neue'
+                ),
+              ),
+            ],
+          )
+      ),
+    );
+  }
+}
+
+class Garis extends StatelessWidget {
+  const Garis({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Divider(
+      height: 20,
+      endIndent: 80,
+      thickness: 2,
+      color: Color(0xFFF6B17A),
+    );
+  }
+}
+
+
