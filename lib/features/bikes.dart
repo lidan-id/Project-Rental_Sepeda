4e8e206607ba55673e997101d52d7af7093d9da7
Kevin Lidan <lidannn012@gmail.com>
2024-05-04 15:17:41 +0700

    Menyelesaikan halaman daftar sepeda

--- lib/features/bikes.dart
+++ lib/features/bikes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_application_1/provider/provider.dart';
 import 'package:provider/provider.dart';
 
@@ -29,20 +30,211 @@ class _BikesState extends State<Bikes> {
                 'City Bikes',
                 style: TextStyle(color: Color(0xFFF6B17A), fontSize: 17),
               ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: Provider.of<BikesProvider>(context)
-                      .cityBikes
-                      .map((e) => Container(
-                            height: 100,
-                            width: 100,
-                            color: Colors.red,
-                            child: Text(e.name),
-                          ))
-                      .toList(),
-                ),
-              )
+              SizedBox(
+                height: 10,
+              ),
+              Column(
+                children: Provider.of<BikesProvider>(context)
+                    .cityBikes
+                    .map((e) => Column(children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            color: Color(0xFFF424769),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  height: 100,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: AssetImage(
+                                              'assets/${e.picture}'))),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Name : " + e.name,
+                                        style:
+                                            TextStyle(color: Color(0xFFF6B17A)),
+                                      ),
+                                      Text('Price : ' + e.price,
+                                          style: TextStyle(
+                                              color: Color(0xFFF6B17A)))
+                                    ])
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          )
+                        ]))
+                    .toList(),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Mountain Bikes',
+                style: TextStyle(color: Color(0xFFF6B17A), fontSize: 17),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Column(
+                children: Provider.of<BikesProvider>(context)
+                    .mountainBikes
+                    .map((e) => Column(children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            color: Color(0xFFF424769),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  height: 100,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: AssetImage(
+                                              'assets/${e.picture}'))),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Name : " + e.name,
+                                        style:
+                                            TextStyle(color: Color(0xFFF6B17A)),
+                                      ),
+                                      Text('Price : ' + e.price,
+                                          style: TextStyle(
+                                              color: Color(0xFFF6B17A)))
+                                    ])
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          )
+                        ]))
+                    .toList(),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Road Bikes',
+                style: TextStyle(color: Color(0xFFF6B17A), fontSize: 17),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Column(
+                children: Provider.of<BikesProvider>(context)
+                    .roadBikes
+                    .map((e) => Column(children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            color: Color(0xFFF424769),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  height: 100,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: AssetImage(
+                                              'assets/${e.picture}'))),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Name : " + e.name,
+                                        style:
+                                            TextStyle(color: Color(0xFFF6B17A)),
+                                      ),
+                                      Text('Price : ' + e.price,
+                                          style: TextStyle(
+                                              color: Color(0xFFF6B17A)))
+                                    ])
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          )
+                        ]))
+                    .toList(),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Sport Bikes',
+                style: TextStyle(color: Color(0xFFF6B17A), fontSize: 17),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Column(
+                children: Provider.of<BikesProvider>(context)
+                    .sportBikes
+                    .map((e) => Column(children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            color: Color(0xFFF424769),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  height: 100,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: AssetImage(
+                                              'assets/${e.picture}'))),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Name : " + e.name,
+                                        style:
+                                            TextStyle(color: Color(0xFFF6B17A)),
+                                      ),
+                                      Text('Price : ' + e.price,
+                                          style: TextStyle(
+                                              color: Color(0xFFF6B17A)))
+                                    ])
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          )
+                        ]))
+                    .toList(),
+              ),
             ],
           ),
         ),
