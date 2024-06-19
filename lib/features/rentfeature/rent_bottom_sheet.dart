420ce5dc444b6e9be1e8227236fa45aad048553f
221110019 <221110019@students.mikroskil.ac.id>
2024-06-20 05:42:53 +0700

    start rent Bottom Sheet

---
+++ lib/features/rentfeature/rent_bottom_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RentBottomSheet extends StatelessWidget {
+  const RentBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
