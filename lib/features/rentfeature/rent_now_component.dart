ef8d44602043e21e349807fc403b8e2099168915
221110019 <221110019@students.mikroskil.ac.id>
2024-06-23 17:59:23 +0700

    Merevisi Rent Now dan Validasi Rent Later

--- lib/features/rentfeature/rent_now_component.dart
+++ lib/features/rentfeature/rent_now_component.dart
@@ -23,10 +23,15 @@ class _RentNowComponentState extends State<RentNowComponent> {
     setState(() {
       if (_selectedOption == 'Hour(s)') {
         _hargaBayar = _durasi * eachbike.price * 1;
-      } else if (_selectedOption == 'Day(s)') {
+        return;
+      }
+      if (_selectedOption == 'Day(s)') {
         _hargaBayar = _durasi * eachbike.price * 24;
-      } else if (_selectedOption == 'Week(s)') {
+        return;
+      }
+      if (_selectedOption == 'Week(s)') {
         _hargaBayar = _durasi * eachbike.price * 168;
+        return;
       }
     });
   }
@@ -47,104 +52,7 @@ class _RentNowComponentState extends State<RentNowComponent> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          TextFormField(
-            onChanged: (value) {
-              setState(() {
-                _durasi = double.tryParse(value) ?? 0;
-                _updateHargaBayar(widget.eachbike);
-              });
-            },
-            cursorColor: const Color(0xFF2D3250),
-            style: const TextStyle(
-                color: Color(0xFF2D3250), fontFamily: "Neue", letterSpacing: 3),
-            keyboardType: TextInputType.number,
-            inputFormatters: [
-              FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
-            ],
-            decoration: InputDecoration(
-                suffixText: _selectedOption,
-                labelText: 'Duration',
-                labelStyle: const TextStyle(
-                    color: Color(0xFF2D3250), fontFamily: "Neue"),
-                prefixIcon: const Icon(
-                  Icons.hourglass_bottom_outlined,
-                  color: Color(0xFFF6B17A),
-                ),
-                hintStyle: const TextStyle(
-                    color: Color(0xFFF6B17A), fontFamily: "Neue"),
-                enabledBorder: const UnderlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFFF6B17A))),
-                focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFFF6B17A)))),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter rent duration';
-              }
-              if (_hargaBayar > balance.saldo) {
-                return "Not enough balance. Please top-up or adjust";
-              }
-              return null;
-            },
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          DropdownButtonFormField<String>(
-            value: _selectedOption,
-            dropdownColor: const Color(0xFF2D3250),
-            icon: const Icon(
-              Icons.arrow_drop_down,
-              color: Color(0xFFF6B17A),
-            ),
-            items: <String>['Hour(s)', 'Day(s)', 'Week(s)'].map((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(
-                  value,
-                  style: const TextStyle(
-                      color: Color(0xFFF6B17A),
-                      fontFamily: "Neue",
-                      letterSpacing: 3),
-                ),
-              );
-            }).toList(),
-            decoration: const InputDecoration(
-              labelText: 'Select Time Units',
-              labelStyle:
-                  TextStyle(color: Color(0xFF2D3250), fontFamily: "Neue"),
-              prefixIcon: Icon(
-                Icons.timer_outlined,
-                color: Color(0xFFF6B17A),
-              ),
-              hintStyle:
-                  TextStyle(color: Color(0xFFF6B17A), fontFamily: "Neue"),
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Color(0xFFF6B17A)),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Color(0xFFF6B17A)),
-              ),
-            ),
-            onChanged: (String? newValue) {
-              setState(() {
-                _selectedOption = newValue!;
-                _updateHargaBayar(widget.eachbike);
-              });
-            },
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please select an option';
-              }
-              if (_hargaBayar > balance.saldo) {
-                return "Not enough balance. Please top-up or adjust";
-              }
-
-              return null;
-            },
-          ),
-          const SizedBox(
-            height: 20,
-          ),
+          durationTextField(balance),
           Center(
             child: Text(
               "Rp${_hargaBayar.toString()}",
@@ -181,6 +89,109 @@ class _RentNowComponentState extends State<RentNowComponent> {
           ),
         ],
       ),
+    );
+  }
+
+  Column durationTextField(SaldoProvider balance) {
+    return Column(
+      children: [
+        TextFormField(
+          onChanged: (value) {
+            setState(() {
+              _durasi = double.tryParse(value) ?? 0;
+              _updateHargaBayar(widget.eachbike);
+            });
+          },
+          cursorColor: const Color(0xFF2D3250),
+          style: const TextStyle(
+              color: Color(0xFF2D3250), fontFamily: "Neue", letterSpacing: 3),
+          keyboardType: TextInputType.number,
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
+          ],
+          decoration: InputDecoration(
+              suffixText: _selectedOption,
+              labelText: 'Duration',
+              labelStyle:
+                  const TextStyle(color: Color(0xFF2D3250), fontFamily: "Neue"),
+              prefixIcon: const Icon(
+                Icons.hourglass_bottom_outlined,
+                color: Color(0xFFF6B17A),
+              ),
+              hintStyle:
+                  const TextStyle(color: Color(0xFFF6B17A), fontFamily: "Neue"),
+              enabledBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFFF6B17A))),
+              focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFFF6B17A)))),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter rent duration';
+            }
+            if (_hargaBayar > balance.saldo) {
+              return "Not enough balance. Please top-up or adjust";
+            }
+            return null;
+          },
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        DropdownButtonFormField<String>(
+          value: _selectedOption,
+          dropdownColor: const Color(0xFF2D3250),
+          icon: const Icon(
+            Icons.arrow_drop_down,
+            color: Color(0xFFF6B17A),
+          ),
+          items: <String>['Hour(s)', 'Day(s)', 'Week(s)'].map((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(
+                value,
+                style: const TextStyle(
+                    color: Color(0xFFF6B17A),
+                    fontFamily: "Neue",
+                    letterSpacing: 3),
+              ),
+            );
+          }).toList(),
+          decoration: const InputDecoration(
+            labelText: 'Select Time Units',
+            labelStyle: TextStyle(color: Color(0xFF2D3250), fontFamily: "Neue"),
+            prefixIcon: Icon(
+              Icons.timer_outlined,
+              color: Color(0xFFF6B17A),
+            ),
+            hintStyle: TextStyle(color: Color(0xFFF6B17A), fontFamily: "Neue"),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Color(0xFFF6B17A)),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Color(0xFFF6B17A)),
+            ),
+          ),
+          onChanged: (String? newValue) {
+            setState(() {
+              _selectedOption = newValue!;
+              _updateHargaBayar(widget.eachbike);
+            });
+          },
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please select an option';
+            }
+            if (_hargaBayar > balance.saldo) {
+              return "Not enough balance. Please top-up or adjust";
+            }
+
+            return null;
+          },
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+      ],
     );
   }
 }
