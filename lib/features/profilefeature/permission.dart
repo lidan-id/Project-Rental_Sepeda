71632f1a48305ba9c2e4d6a06dd4c9b4600e93cc
Kevin Lidan <lidannn012@gmail.com>
2024-07-07 19:33:10 +0700

    mengetes bug 1

--- lib/features/profilefeature/permission.dart
+++ lib/features/profilefeature/permission.dart
@@ -9,87 +9,85 @@ class PermissionManager extends StatelessWidget {
   Widget build(BuildContext context) {
     final permissionProvider = Provider.of<PermissionProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF2D3250),
-        title: const Text(
-          "Permission Manager",
-          style: TextStyle(
-              fontWeight: FontWeight.w900,
-              color: Colors.white,
-              fontFamily: 'Neue',
-              fontSize: 35),
-        ),
-      ),
-      body: Container(
-          padding: const EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            border: Border.all(width: 1),
-            borderRadius: BorderRadius.circular(25),
+        appBar: AppBar(
+          title: const Text(
+            "Permission Manager",
+            style: TextStyle(fontFamily: 'Neue'),
           ),
-          child: Column(
-            children: [
-              SwitchListTile(
-                title: const Text(
-                  'Camera',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontFamily: 'Neue',
-                    color: Color(0xFFF6B17A),
+          foregroundColor: const Color(0xFFF6B17A),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              border: Border.all(width: 1),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Column(
+              children: [
+                SwitchListTile(
+                  title: const Text(
+                    'Camera',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontFamily: 'Neue',
+                      color: Color(0xFFF6B17A),
+                    ),
                   ),
+                  value: permissionProvider.cameraEnabled,
+                  activeColor: const Color(0xFFF6B17A),
+                  onChanged: (value) {
+                    permissionProvider.toggleCamera();
+                  },
                 ),
-                value: permissionProvider.cameraEnabled,
-                activeColor: const Color(0xFFF6B17A),
-                onChanged: (value) {
-                  permissionProvider.toggleCamera();
-                },
-              ),
-              SwitchListTile(
-                title: const Text(
-                  'Location',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontFamily: 'Neue',
-                    color: Color(0xFFF6B17A),
+                SwitchListTile(
+                  title: const Text(
+                    'Location',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontFamily: 'Neue',
+                      color: Color(0xFFF6B17A),
+                    ),
                   ),
+                  value: permissionProvider.locationEnabled,
+                  activeColor: const Color(0xFFF6B17A),
+                  onChanged: (value) {
+                    permissionProvider.toggleLocation();
+                  },
                 ),
-                value: permissionProvider.locationEnabled,
-                activeColor: const Color(0xFFF6B17A),
-                onChanged: (value) {
-                  permissionProvider.toggleLocation();
-                },
-              ),
-              SwitchListTile(
-                title: const Text(
-                  'Microphone',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontFamily: 'Neue',
-                    color: Color(0xFFF6B17A),
+                SwitchListTile(
+                  title: const Text(
+                    'Microphone',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontFamily: 'Neue',
+                      color: Color(0xFFF6B17A),
+                    ),
                   ),
+                  value: permissionProvider.microphoneEnabled,
+                  activeColor: const Color(0xFFF6B17A),
+                  onChanged: (value) {
+                    permissionProvider.toggleMicrophone();
+                  },
                 ),
-                value: permissionProvider.microphoneEnabled,
-                activeColor: const Color(0xFFF6B17A),
-                onChanged: (value) {
-                  permissionProvider.toggleMicrophone();
-                },
-              ),
-              SwitchListTile(
-                title: const Text(
-                  'History',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontFamily: 'Neue',
-                    color: Color(0xFFF6B17A),
+                SwitchListTile(
+                  title: const Text(
+                    'History',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontFamily: 'Neue',
+                      color: Color(0xFFF6B17A),
+                    ),
                   ),
+                  value: permissionProvider.historyEnabled,
+                  activeColor: const Color(0xFFF6B17A),
+                  onChanged: (value) {
+                    permissionProvider.toggleHistory();
+                  },
                 ),
-                value: permissionProvider.historyEnabled,
-                activeColor: const Color(0xFFF6B17A),
-                onChanged: (value) {
-                  permissionProvider.toggleHistory();
-                },
-              ),
-            ],
-          ))
+              ],
+            ))
         // child: Column(
         //   children: [
         //     SwitchListTile(
